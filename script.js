24ff3dba47521f0ec4594e6068761dceb089ecc7
Nick C <nickchenschool@gmail.com>
2024-08-26 15:29:12 -0700

    added scoreboard funcitonality and added a display message below

--- script.js
+++ script.js
@@ -1,37 +1,94 @@
 const choices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
-const results = document.getElementById("game-results");
-const cScore = document.getElementById("computerScore");
-const hScore = document.getElementById("humanScore");
-const replayButton = document.getElementById("play-again");
+const rockButton = document.getElementById("rock-btn");
+const paperButton = document.getElementById("paper-btn");
+const scissorsButton = document.getElementById("scissors-btn");
+const scoreboard = document.getElementById("scoreboard");
+const cScore = document.getElementById("computer-score");
+const hScore = document.getElementById("human-score");
 
 function getComputerChoice() {
   const roll = Math.floor(Math.random() * 3);
   return choices[roll];
 }
 
-function getHumanChoice() {
-  let playerInput = prompt(("Choose either: ROCK, PAPER, or SCISSORS"));
-  let player = playerInput.toLowerCase();
-  if (checkInput(player)) {
-    return player;
-  }
-  else {
-    alert(`Please put in correct input`);
-    return getHumanChoice();
+rockButton.addEventListener("click", () => {
+  playRound("rock", computerChoice);
+})
+
+paperButton.addEventListener("click", () => {
+  playRound("paper", computerChoice);
+})
+
+scissorsButton.addEventListener("click", () => {
+  playRound("scissors", computerChoice);
+})
+
+function playRound(humanChoice, computerChoice) {
+  switch (humanChoice) {
+    case "rock" : 
+      if (computerChoice === humanChoice) {
+        humanScore++;
+        computerScore++;
+        updateScores();
+        displayMsg(`You Tie! You both chose <span class="result-style">${humanChoice}<span>`);
+      }
+      else if (computerChoice === "paper") {
+        computerScore++;
+        updateScores();
+        displayMsg(`You Lose... Computer chose <span class="lose-style">Paper</span>`);
+      }
+      else {
+        humanScore++;
+        updateScores();
+        displayMsg(`You Win! You chose <span class="win-style">${humanChoice}</span>, which beats <span class="lose-style">${computerChoice}</span>!`);
+      }
+    case "paper" : 
+      if (computerChoice === humanChoice) {
+        humanScore++;
+        computerScore++;
+        updateScores();
+        displayMsg(`You Tie! You both chose <span class="result-style">${humanChoice}<span>`);
+      }
+      else if (computerChoice === "scissors") {
+        computerScore++;
+        updateScores();
+        displayMsg(`You Lose... Computer chose <span class="lose-style">Scissors</span>`);
+      }
+      else {
+        humanScore++;
+        updateScores();
+        displayMsg(`You Win! You chose <span class="win-style">${humanChoice}</span>, which beats <span class="lose-style">${computerChoice}</span>!`);
+      }
+    case "scissors" : 
+      if (computerChoice === humanChoice) {
+        humanScore++;
+        computerScore++;
+        updateScores();
+        displayMsg(`You Tie! You both chose <span class="result-style">${humanChoice}<span>`);
+      }
+      else if (computerChoice === "rock") {
+        computerScore++;
+        updateScores();
+        displayMsg(`You Lose... Computer chose <span class="lose-style">${computerChoice}</span>`);
+      }
+      else {
+        humanScore++;
+        updateScores();
+        displayMsg(`You Win! You chose <span class="win-style">${humanChoice}</span>, which beats <span class="lose-style">${computerChoice}</span>!`);
+      }
   }
 }
 
-function checkInput(input) {
-  let valid = false;
-  for (const choice of choices) {
-    if (choice === input) {
-      valid = true;
-    }
-  }
-  return valid;
+function updateScores() {
+  hScore.innerText = `Player: ${humanScore}`;
+  cScore.innerText = `Computer: ${computerScore}`;
 }
 
-playGame();
-
+function displayMsg(message) {
+  const resultMsg = document.createElement("h1");
+  resultMsg.innerHTML = message;
+  resultMsg.style = "color: yellow; font-family: Impact";
+  scoreboard.appendChild(resultMsg);
+}
